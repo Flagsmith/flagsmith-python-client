07a6bfdaf3106f942a8f970b15cc17286a13782d
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-10-04 12:46:16 +0530

    minor refactoring and docs

--- flagsmith/analytics.py
+++ flagsmith/analytics.py
@@ -1,7 +1,9 @@
+import copy
 import logging
 from collections import UserDict
 from datetime import datetime
 
+import requests
 from requests_futures.sessions import FuturesSession
 
 logger = logging.getLogger(__name__)
@@ -11,7 +13,7 @@ ANALYTICS_ENDPOINT = "analytics/flags/"
 # Used to control how often we send data(in seconds)
 ANALYTICS_TIMER = 10
 
-session = FuturesSession()
+session = FuturesSession(max_workers=4)
 
 
 class AnalyticsProcessor:
@@ -20,33 +22,43 @@ class AnalyticsProcessor:
     docs: https://docs.flagsmith.com/advanced-use/flag-analytics
     """
 
-    def __init__(self, environment_key: str, base_api_url: str):
+    def __init__(self, environment_key: str, base_api_url: str, timeout: int = 3):
         """
         Initialise Flagsmith environment.
 
-        :param environment_id: environment key obtained from the Flagsmith UI
-        :param api:  api url to override when using self hosted version
+        :param environment_key: environment key obtained from the Flagsmith UI
+        :param  base_api_url:  base api url to override when using self hosted version
+        :param timeout: used to tell requests to stop waiting for a response after a given number of seconds
         """
         self.analytics_endpoint = base_api_url + ANALYTICS_ENDPOINT
         self.environment_key = environment_key
         self._last_flushed = datetime.now()
         self.analytics_data = {}
+        self.timeout = timeout
         super().__init__()
 
     def flush(self):
-        # Sends all the collected data to the api asynchronously and resets the timer
+        """
+        Sends all the collected data to the api asynchronously and resets the timer
+        """
+
+        if not self.analytics_data:
+            return
         try:
+            # create a copy of the `analytics_data` because we call `clear` on the dictionary later
+            # and for the obvious reason when mocking we get the empty dict.
+            data = copy.copy(self.analytics_data)
             session.post(
                 self.analytics_endpoint,
-                data=self.analytics_data,
-                timeout=5,
+                data=data,
+                timeout=self.timeout,
                 headers={"X-Environment-Key": self.environment_key},
             )
 
             self.analytics_data.clear()
             self._last_flushed = datetime.now()
 
-        except Exception as e:
+        except requests.RequestException as e:
             logger.error("Failed to send anaytics data. Error message was %s" % e)
 
     def track_feature(self, feature_id: int):
