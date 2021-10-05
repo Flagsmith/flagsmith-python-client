52efd24754f80078fe536246fd79e5b980dc2595
Matthew Elwell <matthewe@solidstategroup.com>
2021-10-05 18:27:40 +0100

    Tidy up docstrings

--- flagsmith/analytics.py
+++ flagsmith/analytics.py
@@ -13,17 +13,19 @@ session = FuturesSession(max_workers=4)
 
 class AnalyticsProcessor:
     """
-    AnalyticsProcessor is used to track how often individual Flags are evaluated within the Flagsmith SDK
-    docs: https://docs.flagsmith.com/advanced-use/flag-analytics
+    AnalyticsProcessor is used to track how often individual Flags are evaluated within
+    the Flagsmith SDK. Docs: https://docs.flagsmith.com/advanced-use/flag-analytics.
     """
 
     def __init__(self, environment_key: str, base_api_url: str, timeout: int = 3):
         """
-        Initialise Flagsmith environment.
+        Initialise the AnalyticsProcessor to handle sending analytics on flag usage to
+        the Flagsmith API.
 
         :param environment_key: environment key obtained from the Flagsmith UI
-        :param  base_api_url:  base api url to override when using self hosted version
-        :param timeout: used to tell requests to stop waiting for a response after a given number of seconds
+        :param base_api_url: base api url to override when using self hosted version
+        :param timeout: used to tell requests to stop waiting for a response after a
+            given number of seconds
         """
         self.analytics_endpoint = base_api_url + ANALYTICS_ENDPOINT
         self.environment_key = environment_key
