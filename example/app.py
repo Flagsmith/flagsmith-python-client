b2ee745249b6f92f46a1ff919a5686ec73ff8017
Matthew Elwell <matthewe@solidstategroup.com>
2022-01-27 14:40:40 +0000

    Tidy up example app

--- example/app.py
+++ example/app.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from flagsmith import Flagsmith
 from flagsmith.models import DefaultFlag
@@ -11,8 +11,10 @@ app = Flask(__name__)
 flagsmith = Flagsmith(
     environment_key=os.environ.get("FLAGSMITH_ENVIRONMENT_KEY"),
     defaults=[
+        # Set a default flag which will be used if the "secret_button"
+        # feature is not returned by the API
         DefaultFlag(
-            enabled=True,
+            enabled=False,
             value=json.dumps({"colour": "#b8b8b8"}),
             feature_name="secret_button",
         )
@@ -20,15 +22,32 @@ flagsmith = Flagsmith(
 )
 
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.args:
+        identifier = request.args["identifier"]
+
+        trait_key = request.args.get("trait-key")
+        trait_value = request.args.get("trait-value")
+        traits = {trait_key: trait_value} if trait_key else None
+
+        # Get the flags for an identity, including the provided trait which will be
+        # persisted to the API for future requests.
+        identity_flags = flagsmith.get_identity_flags(
+            identifier=identifier, traits=traits
+        )
+        show_button = identity_flags.is_feature_enabled("secret_button")
+        button_data = json.loads(identity_flags.get_feature_value("secret_button"))
+        return render_template(
+            "home.html",
+            show_button=show_button,
+            button_colour=button_data["colour"],
+            identifier=identifier,
+        )
+
+    # Get the default flags for the current environment
     flags = flagsmith.get_environment_flags()
-    identity_flags = flagsmith.get_identity_flags("identifier")
-
-    show_button = flags.is_feature_enabled(
-        "secret_button"
-    ) and identity_flags.is_feature_enabled("secret_button")
-
+    show_button = flags.is_feature_enabled("secret_button")
     button_data = json.loads(flags.get_feature_value("secret_button"))
 
     return render_template(
--- example/readme.md
+++ example/readme.md
@@ -11,11 +11,12 @@ need to go through the following steps:
 {"colour": "#ababab"}
 ```
 
-4. Update the .env file located in this directory with the environment key of one of the environments in flagsmith (
-This can be found on the 'settings' page accessed from the menu on the left under the chosen environment.)
+4. Create a .env file from the template located in this directory with the environment key of one of the environments 
+in flagsmith (This can be found on the 'settings' page accessed from the menu on the left under the chosen environment.)
 5. From a terminal window, export those environment variables (either manually or by using `export $(cat .env)`)
 6. Run the app using `flask run`
 7. Browse to http://localhost:5000
 
 Now you can play around with the 'secret_button' feature in flagsmith, turn it on to show it and edit the colour in the
-json value to edit the colour of the button. 
+json value to edit the colour of the button. You can also identify as a given user and then update the settings for the
+secret button feature for that user in the flagsmith interface to see the affect that has too. 
