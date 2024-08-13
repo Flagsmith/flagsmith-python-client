e11c7275a94be8d838242fd0445b081cbf3d2a33
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-08-13 21:18:12 +0100

    use static values

--- tests/test_local_eval.py
+++ tests/test_local_eval.py
@@ -28,16 +28,6 @@ def identifiers() -> Tuple[str, ...]:
     return ("1", "2", "3", "4", "5")
 
 
-def get_matching_str(*values: str) -> str:
-    class _matching_str(str):
-        _values = values
-
-        def __eq__(self, other: object) -> bool:
-            return other in self._values
-
-    return _matching_str(" / ".join(values))
-
-
 def _get_flag_mock(**kwargs: object) -> mock.Mock:
     mock_kwargs = {field.name: mock.ANY for field in fields(Flag)}
     mock_kwargs.update(kwargs)
@@ -47,49 +37,15 @@ def _get_flag_mock(**kwargs: object) -> mock.Mock:
 
 
 @pytest.fixture
-def environment_default_or_percentage_split_value_flag_mock() -> mock.Mock:
-    return _get_flag_mock(
-        value=get_matching_str(
-            "environment default",
-            'overridden by "split_segment"',
-        ),
-    )
-
-
-@pytest.fixture
-def mv_value_flag_mock() -> mock.Mock:
-    return _get_flag_mock(
-        value=get_matching_str(
-            "a",
-            "b",
-        ),
-    )
-
-
-@pytest.fixture
-def mv_segment_override_value_flag_mock() -> mock.Mock:
-    return _get_flag_mock(
-        value=get_matching_str(
-            'overridden by segment "power_users"',
-            "a",
-        ),
-    )
-
-
-@pytest.fixture
-def expected_flags_without_identity_overrides(
-    environment_default_or_percentage_split_value_flag_mock: mock.Mock,
-    mv_segment_override_value_flag_mock: mock.Mock,
-    mv_value_flag_mock: mock.Mock,
-) -> Dict[str, Flags]:
+def expected_flags_without_identity_overrides() -> Dict[str, Flags]:
     return {
         # - [ ]  Identity 1:
         #   - [ ]  multivariate feature: % split
         #   - [ ]  normal feature: environment default / % split override
         "1": Flags(
             {
-                "multivariate_feature": mv_value_flag_mock,
-                "normal_feature": environment_default_or_percentage_split_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(value="b"),
+                "normal_feature": _get_flag_mock(value='overridden by "split_segment"'),
             },
         ),
         # - [ ]  Identity 2:
@@ -97,8 +53,8 @@ def expected_flags_without_identity_overrides(
         #   - [ ]  normal feature: environment default / % split override
         "2": Flags(
             {
-                "multivariate_feature": mv_value_flag_mock,
-                "normal_feature": environment_default_or_percentage_split_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(value="b"),
+                "normal_feature": _get_flag_mock(value="environment default"),
             },
         ),
         # - [ ]  Identity 3:
@@ -106,8 +62,8 @@ def expected_flags_without_identity_overrides(
         #   - [ ]  normal feature: environment default / % split override
         "3": Flags(
             {
-                "multivariate_feature": mv_segment_override_value_flag_mock,
-                "normal_feature": environment_default_or_percentage_split_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(value="a"),
+                "normal_feature": _get_flag_mock(value="environment default"),
             },
         ),
         # - [ ]  Identity 4:
@@ -115,8 +71,8 @@ def expected_flags_without_identity_overrides(
         #   - [ ]  normal feature: environment default / % split override
         "4": Flags(
             {
-                "multivariate_feature": mv_value_flag_mock,
-                "normal_feature": environment_default_or_percentage_split_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(value="b"),
+                "normal_feature": _get_flag_mock(value='overridden by "split_segment"'),
             },
         ),
         # - [ ]  Identity 5:
@@ -124,19 +80,15 @@ def expected_flags_without_identity_overrides(
         #   - [ ]  normal feature: environment default / % split override
         "5": Flags(
             {
-                "multivariate_feature": mv_value_flag_mock,
-                "normal_feature": environment_default_or_percentage_split_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(value="a"),
+                "normal_feature": _get_flag_mock(value='overridden by "split_segment"'),
             },
         ),
     }
 
 
 @pytest.fixture
-def expected_flags_with_identity_overrides(
-    environment_default_or_percentage_split_value_flag_mock: mock.Mock,
-    mv_segment_override_value_flag_mock: mock.Mock,
-    mv_value_flag_mock: mock.Mock,
-) -> Dict[str, Flags]:
+def expected_flags_with_identity_overrides() -> Dict[str, Flags]:
     return {
         # - [ ]  Identity 1:
         #   - [ ]  multivariate feature: identity override
@@ -152,7 +104,7 @@ def expected_flags_with_identity_overrides(
         #   - [ ]  normal feature: identity override
         "2": Flags(
             {
-                "multivariate_feature": mv_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(value="b"),
                 "normal_feature": _get_flag_mock(value="overridden for 2"),
             },
         ),
@@ -161,7 +113,9 @@ def expected_flags_with_identity_overrides(
         #   - [ ]  normal feature: identity override
         "3": Flags(
             {
-                "multivariate_feature": mv_segment_override_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(
+                    value='overridden by segment "power_users"'
+                ),
                 "normal_feature": _get_flag_mock(value="overridden for 3"),
             },
         ),
@@ -170,8 +124,10 @@ def expected_flags_with_identity_overrides(
         #   - [ ]  normal feature: environment default / % split override
         "4": Flags(
             {
-                "multivariate_feature": mv_segment_override_value_flag_mock,
-                "normal_feature": environment_default_or_percentage_split_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(
+                    value='overridden by segment "power_users"'
+                ),
+                "normal_feature": _get_flag_mock(value='overridden by "split_segment"'),
             },
         ),
         # - [ ]  Identity 5:
@@ -179,8 +135,8 @@ def expected_flags_with_identity_overrides(
         #   - [ ]  normal feature: environment default / % split override
         "5": Flags(
             {
-                "multivariate_feature": mv_value_flag_mock,
-                "normal_feature": environment_default_or_percentage_split_value_flag_mock,
+                "multivariate_feature": _get_flag_mock(value="b"),
+                "normal_feature": _get_flag_mock(value='overridden by "split_segment"'),
             },
         ),
     }
